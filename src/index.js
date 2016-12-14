852036bf3f697a774843303a91d5a5726cd8b196
Snapko Mikalai <kolyan@kolyan.instinctools.ru>
2016-12-14 09:06:37 +0300

    init redux commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider, connect} from 'react-redux';
 import App from './App';
 import './index.css';
-import clipbeardStore from './store/clipbeardStore';
+import Store from './store/Store';
 
 const AppContainer = connect()(App);
 
 ReactDOM.render(
-  <Provider store={ clipbeardStore }>
+  <Provider store={ Store }>
     <AppContainer />
   </ Provider>,
   document.getElementById('root')
